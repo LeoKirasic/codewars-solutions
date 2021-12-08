144ed505ba6e5059c1c771c15e34950b48bd1adf
LeoKirasic <kirasicleo@gmail.com>
2021-12-08 16:59:31 +0100

    Add solution for the array diff exercise

---
+++ 6kyu/arrayDiff.js
@@ -0,0 +1,13 @@
+function arrayDiff(a, b) {
+  for (let i = 0; i < a.length; i++) {
+    for (let j = 0; j < b.length; j++) {
+      if (a[i] === b[j]) {
+        a.splice(i, 1);
+        if (a[i] === b[j]) {
+          a.splice(i, 1);
+        }
+      }
+    }
+  }
+  return a;
+}
